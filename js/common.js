587dea20389e0b8d5c4a702d7f8e865729ea0562
jackeiw <jackeiw@163.com>
2018-06-08 08:55:16 +0800

    modify

--- js/common.js
+++ js/common.js
@@ -1,8 +1,10 @@
-(function(e){
-    var V = {};
-    V.say = function(msg){
+(function(e) {
+    var V = { "version": "0.0.1", "author": "vincent" };
+    V.say = function(msg) {
         alert(msg);
     };
+
+
 
     window.v = V;
 })(window);
