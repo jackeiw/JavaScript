69b98b88c8777db46f5b207766ce4692cc8e89db
Vincent <jackeiw@163.com>
2022-03-19 11:20:29 +0800

    测试静态方法和实例方法。

--- old/HTML5/js/test/prototype.js
+++ old/HTML5/js/test/prototype.js
@@ -6,6 +6,10 @@ MyClass.prototype.sayHello = function(name){
     alert(this.name+" prototype say: hello,"+name);
 }
 
+MyClass.StaticSay = function(name, msg){
+    alert(" Static say: hello," + name + '!' + msg);
+}
+
 var myObject = {};
 myObject.sayHello = function(name){
     alert('hello'+name);
